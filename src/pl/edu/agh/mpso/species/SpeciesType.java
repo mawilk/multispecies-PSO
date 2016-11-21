c4f1a940dba572069d603f2a0fb16b37ab947ef4
Magdalena Wilk <ma.wilk@tlen.pl>
2016-11-21 21:53:03 +0100

    refactor species type enum

--- src/pl/edu/agh/mpso/species/SpeciesType.java
+++ src/pl/edu/agh/mpso/species/SpeciesType.java
@@ -3,89 +3,52 @@ package pl.edu.agh.mpso.species;
 import java.awt.Color;
 
 public enum SpeciesType {
-	ALL(0),
-	GLOBAL_AND_LOCAL(1),
-	GLOBAL_AND_NEIGHBOUR(2),
-	LOCAL_AND_NEIGHBOUR(3),
-	GLOBAL_ONLY(4),
-	LOCAL_ONLY(5),
-	NEIGHBOUR_ONLY(6),
-	RANDOM(7);
+	ALL(1.0, 1.0, 1.0, "Normal"), 
+	GLOBAL_AND_LOCAL(1.0, 1.0, 0.0, "Global and local"), 
+	GLOBAL_AND_NEIGHBOUR(1.0, 0.0, 1.0, "Global and neighbour"), 
+	LOCAL_AND_NEIGHBOUR(0.0, 1.0, 1.0, "Local and neighbour"), 
+	GLOBAL_ONLY(1.0, 0.0, 0.0, "Global only"), 
+	LOCAL_ONLY(0.0, 1.0, 0.0, "Local only"), 
+	NEIGHBOUR_ONLY(0.0, 0.0, 1.0, "Neighbour only"), 
+	RANDOM();
+
+	private static final int SPECIES_NUMBER = 8;
 	
-	public final int type;
+	private final double global;
+	private final double local;
+	private final double neighbour;
+
 	private final Color color;
-	
-	private static final String [] typeNames = new String [] {
-			"Normal", "Global and local", "Global and neighbour", "Local and neighbour", 
-			"Global only", "Local only", "Neighbour only", "Random weights" 
-	};
-	
-	
-	private SpeciesType(int type){
-		this.type = type;
-		int rgb = 255 / (8 - type);
+	private final String label;
+
+	private SpeciesType(double global, double local, double neighbour, String label) {
+		int rgb = 255 / (SPECIES_NUMBER - this.ordinal());
 		this.color = new Color(rgb, rgb, rgb);
+		this.global = global;
+		this.local = local;
+		this.neighbour = neighbour;
+		this.label = label;
 	}
-	
+
+	private SpeciesType() {
+		int rgb = 255 / (SPECIES_NUMBER - this.ordinal());
+		this.label = "Random weights";
+		this.color = new Color(rgb, rgb, rgb);
+		this.global = Math.random() * 3.0;
+		this.local = Math.random() * (3.0 - global);
+		this.neighbour = 3.0 - global - local;
+	}
+
 	@Override
-	public String toString(){
-		return typeNames[type];
+	public String toString() {
+		return label;
 	}
-	
-	public Color getColor(){
+
+	public Color getColor() {
 		return color;
 	}
-	
-	public double [] getWeights(){
-		double local, global, neighbour;
-		
-		switch(type){
-			case 0:
-				global = 1.0;
-				local = 1.0;
-				neighbour = 1.0;
-				break;
-			case 1:
-				global = 1.0;
-				local = 1.0;
-				neighbour = 0.0;
-				break;
-			case 2:
-				global = 1.0;
-				local = 0.0;
-				neighbour = 1.0;
-				break;
-			case 3:
-				global = 0.0;
-				local = 1.0;
-				neighbour = 1.0;
-				break;
-			case 4:
-				global = 1.0;
-				local = 0.0;
-				neighbour = 0.0;
-				break;
-			case 5:
-				global = 0.0;
-				local = 1.0;
-				neighbour = 0.0;
-				break;
-			case 6:
-				global = 0.0;
-				local = 0.0;
-				neighbour = 1.0;
-				break;
-			case 7:
-				global = Math.random() * 3.0;
-				local = Math.random() * (3.0 - global);
-				neighbour = 3.0 - global - local;
-				break;
-			default:
-				global = 1.0;
-				local = 1.0;
-				neighbour = 1.0;
-		}
-		
-		return new double[]{global, local, neighbour};
+
+	public double[] getWeights() {
+		return new double[] { global, local, neighbour };
 	}
 }
